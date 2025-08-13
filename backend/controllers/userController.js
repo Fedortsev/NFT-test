03896e507ec212016aa1781e104a3591626a15ff
Fedortsev <bobikxgendelfx@gmail.com>
2025-08-13 20:13:57 +0000

    backend fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,19 +9,19 @@ const loginUser = async (req,res) =>{
         const user = await userModel.findOne({email});
 
         if(!user){
-           return res.json({success:false, message:'User does not exist'}) 
+           return res.status(401).json({success:false, message:'User does not exist'}) 
         }
         const isMatch = await bcrypt.compare(password,user.password)
 
         if(!isMatch){
-            return res.json({success:false, message:'Invalid credentials'})
+            return res.status(401).json({success:false, message:'Invalid credentials'})
         }
 
         const token = createToken(user._id);
         res.json({success:true, token})
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:'Error'})
+        res.status(500).json({success:false, message:'Error'})
     }
 }
 
@@ -37,7 +37,7 @@ const registerUser = async (req, res) =>{
         // checking is user already exists
         const exists = await userModel.findOne({email});
         if(exists){
-            return res.json({success:false, message:'User already exists'})
+            return res.status(401).json({success:false, message:'User already exists'})
         }
 
         //validating email format and strong password
