9d7cb523fb20518cce2ca29ac56cf069f635e761
Fedortsev <bobikxgendelfx@gmail.com>
2025-08-13 22:44:04 +0000

    fix: Hot fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,7 @@ const registerUser = async (req, res) =>{
 
       const user =  await newUser.save()
       const token = createToken(user._id)
-      res.json({success:true, token})
+      res.json({success:true, token, role:user.role})
 
     } catch (error) {
         console.log(error)
